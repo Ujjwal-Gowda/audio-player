dcd204ad87cf8f034111e85dafec35402329d619
ujjwal G <crystalninja0123@gmail.com>
2025-10-02 23:31:42 +0530

    protectroute

---
+++ frontend/src/component/protectedRoute.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/authcontext";
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const auth = useContext(AuthContext);
+
+  if (!auth?.token) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
