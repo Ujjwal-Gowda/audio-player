bac1e98d852f43e50884115662b244858ad5a54e
ujjwal G <crystalninja0123@gmail.com>
2025-10-14 15:59:36 +0530

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,14 +11,24 @@ dotenv.config();
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "http://localhost:5174",
+//       "http://localhost:5175",
+//       "audio-player-five-coral.vercel.app",
+//     ],
+//     credentials: true,
+//   }),
+// );
+//
+// ✅ Configure CORS properly
 app.use(
   cors({
-    origin: [
-      // "http://localhost:5173",
-      // "http://localhost:5174",
-      // "http://localhost:5175",
-      "audio-player-five-coral.vercel.app",
-    ],
+    origin: ["audio-player-five-coral.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
